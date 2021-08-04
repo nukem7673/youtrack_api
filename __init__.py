7c031761a3e2edbdae49ddab5d1606371f5bd5ad
= <=>
2021-08-04 14:20:33 -0600

    Update README and __init__.py comments

--- __init__.py
+++ __init__.py
@@ -17,21 +17,27 @@ write_to_json(issues)
 
 '''YouTrack attachment process:
             1. Each issue has to be queried to see if there are attachments present for it.
-            2. The response will carry a URL which we'll store
+            2.
+                A. The response will carry a URL which we'll store in a json file
+                B. Read in the json output
+                B. Filter responses to only contain those issues WITH attachments
             3. Once the list of issues is processed, we can run CURL commands on the list of attachments
             4. We'll need to reassign these attachments back to the issues using the attchment's stored file locations'''
             
 # 1. Pull ids, run query
 issue_ids = extract_issue_ids(issues)
-# 2. Store attachment-download-url from response
+
+# 2.A. Store attachment-download-url from response
 attachment_urls_and_issue_ids = get_all_attachments(issue_ids)
-# 3. Request downloads for each, store filename and path to be reassociated later
+
+# 2.B. Read-in the returned json
 attachment_issue_map = ""
 with open('attachment_urls_and_issue_ids.json', 'r') as f:
     attachment_issue_map = json.loads(f.read())
-# Remove empties
+# 2.C. Remove empties
 attachment_issue_map = filter(lambda item: item is not None, attachment_issue_map)
+
+# 3. Request downloads for each, store filename and path to be reassociated later
 download_main(attachment_issue_map)
-# # 4. Write out the mapping
-# with open('issue_ids_and_attachments_map.json', 'w') as f:
-#     f.write(json.dumps(attachment_urls_and_issue_ids))
+
+# 4. On your own, but now you have the downloads, issue data, and a map to reconnect the attachments!
